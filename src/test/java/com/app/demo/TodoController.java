e596e61f0067c505353a2095e8aae385f08a3b5f
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 12:03:59 +0500

    TodoController: Injected MockMvc and TodoService dependencies

--- src/test/java/com/app/demo/TodoController.java
+++ src/test/java/com/app/demo/TodoController.java
@@ -1,10 +1,17 @@
 package com.app.demo;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class TodoController {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    private final TodoService todoService;
 }
