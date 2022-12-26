f90b654f7f44a72be227a3815089b45c51ed2adb
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 12:18:51 +0500

    TodoService: Created new method getAllTodos returning List<Todo>

--- src/main/java/com/app/demo/TodoService.java
+++ src/main/java/com/app/demo/TodoService.java
@@ -2,6 +2,10 @@ package com.app.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TodoService {
+    public List<Todo> getAllTodos() {
+    }
 }
