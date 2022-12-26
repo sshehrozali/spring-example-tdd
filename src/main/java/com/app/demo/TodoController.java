4b16d8a5c699932c02fa396a91b047a12b243da4
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 13:23:37 +0500

    TodoController: Created new method getAllTodos returning new ResponseEntity of List<Todos> and HttpStatus.OK TodoController: Added @GetMapping("/todos") annotation to getAllTodos method

--- src/main/java/com/app/demo/TodoController.java
+++ src/main/java/com/app/demo/TodoController.java
@@ -1,10 +1,20 @@
 package com.app.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService todoService;
+
+    @GetMapping("/todos")
+    ResponseEntity<List<Todo>> getAllTodos() {
+        return new ResponseEntity<>(todoService.getAllTodos(), HttpStatus.OK);
+    }
 }
