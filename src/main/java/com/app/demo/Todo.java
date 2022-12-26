2caf4653714c3845fa1ad65e80f04125804ba663
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 12:14:29 +0500

    Todo: Added @AllArgsConstructor, @NoArgsConstructor, @Getter and @Setter annotations

--- src/main/java/com/app/demo/Todo.java
+++ src/main/java/com/app/demo/Todo.java
@@ -1,5 +1,15 @@
 package com.app.demo;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Todo {
     private String title;
     private String note;
