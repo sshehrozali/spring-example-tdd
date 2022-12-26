fb7b8c9c67696ac7eb4c54d22500b31334407cf5
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 13:20:35 +0500

    TodoController: Added @RestController and @RequiredArgsConstructor annotations TodoController: Injected TodoService depedency

--- src/main/java/com/app/demo/TodoController.java
+++ src/main/java/com/app/demo/TodoController.java
@@ -1,4 +1,10 @@
 package com.app.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class TodoController {
+    private final TodoService todoService;
 }
