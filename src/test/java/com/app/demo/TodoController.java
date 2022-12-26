96ff39476a5ae415960a0d73e00c054b651699ce
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 11:48:51 +0500

    TodoController: Added @ExtendWith(SpringExtension.class) annotation

--- src/test/java/com/app/demo/TodoController.java
+++ src/test/java/com/app/demo/TodoController.java
@@ -1,7 +1,10 @@
 package com.app.demo;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class TodoController {
 }
