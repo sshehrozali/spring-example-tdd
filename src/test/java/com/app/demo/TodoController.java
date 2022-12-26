156b38c8778106da2155bcb85fc3ecdd13fca2b9
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 14:26:09 +0500

    TodoController: Added @SpringBootTest annotation to TodoController class

--- src/test/java/com/app/demo/TodoController.java
+++ src/test/java/com/app/demo/TodoController.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,6 +20,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class TodoController {
