1f512505b0d09b0d8f26f5ff936b992f5b1eb925
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-26 12:23:51 +0500

    TodoService: Returned new empty ArrayList<>()

--- src/main/java/com/app/demo/TodoService.java
+++ src/main/java/com/app/demo/TodoService.java
@@ -2,10 +2,12 @@ package com.app.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TodoService {
     public List<Todo> getAllTodos() {
+        return new ArrayList<>();
     }
 }
